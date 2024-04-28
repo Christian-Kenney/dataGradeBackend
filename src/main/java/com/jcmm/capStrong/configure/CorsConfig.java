eb750d599a647af6f4d08c8502dd6c3265b56404
Christian-Kenney <christiankenney@ymail.com>
2024-04-28 16:35:43 -0500

    update

--- src/main/java/com/jcmm/capStrong/configure/CorsConfig.java
+++ src/main/java/com/jcmm/capStrong/configure/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // Add your frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Add allowed methods
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Add allowed methods
                 .allowedHeaders("*"); // Add allowed headers
     }
 }
